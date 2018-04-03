9d2e03414b85a1764b42b2dd82bc80fcfdf192bf
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-04-03 15:06:12 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+
+
+class EyesOnMe extends React.Component{
+  //create a EyesOnMe React Component
+
+  onFocusFunc = () => console.log('Good!')
+  // log good! with focus
+
+
+  onBlurFunc = () => console.log('Hey! Eyes on me!')
+  // log what happens with blur , need function
+
+
+  render() {
+    return(
+    <button onFocus={this.onFocusFunc} onBlur={this.onBlurFunc}></button>
+    // render a button
+    //on button, add event handlers for blur and focus
+    // dont forget (this.) with class
+  )}
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  //create 'Keypad' React Component
+
+  passwordEnteringMessage = () => console.log('Entering password...')
+  // use console.log to print out text entering password
+
+  render() {
+    return (
+        <input type="password" onKeyUp={this.passwordEnteringMessage} />
+        // render input with the right type(password)
+        // event handler on input that listens dor keyup event
+    )
+  }
+}
+
+export default Keypad;
